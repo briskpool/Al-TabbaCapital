242b57c8bc6f2a34bd95fba69e6834070fccc316
Muzammil AK <52231636+briskpool@users.noreply.github.com>
2025-05-17 21:11:32 +0500

    Update terms-condition.php

--- terms-condition.php
+++ terms-condition.php
@@ -268,7 +268,7 @@
                     <h3>21. CALIFORNIA USERS AND RESIDENTS</h3>
 
                     <p>
-                        If any complaint with us is not satisfactorily resolved, you can contact the Complaint Assistance Unit of the Division of Consumer Services of the California Department of Consumer Affairs in writing at 1625 North Market Blvd., Suite N 112, Sacramento, California 95834 or by telephone at (800) 952-5210 or (916) 445-1254.
+                        If any complaint with us is not satisfactorily resolved, you can contact the Complaint Assistance Unit of the Division of Consumer Services of the California Department of Consumer Affairs in writing at 1625 North Market Blvd., Suite N 112, Sacramento, California 95834 or by telephone at 0203 807 1460.
                     </p>
 
                     <h3>22. MISCELLANEOUS</h3>
